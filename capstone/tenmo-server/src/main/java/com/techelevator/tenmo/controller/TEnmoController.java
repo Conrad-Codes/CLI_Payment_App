2d9c78282e4485e743fb88eb44c9049dfa8a127a
Conrad Estes <Conrad.Estes@gmail.com>
2022-04-27 11:11:17 -0400

    Server Check Balance

---
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/controller/TEnmoController.java
@@ -0,0 +1,30 @@
+package com.techelevator.tenmo.controller;
+
+import com.techelevator.tenmo.dao.AccountDao;
+import com.techelevator.tenmo.dao.TransactionDao;
+import com.techelevator.tenmo.model.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@PreAuthorize("isAuthenticated()")
+@RestController
+public class TEnmoController {
+
+    @Autowired
+    AccountDao accountDao;
+
+    @Autowired
+    TransactionDao transactionDao;
+
+    @RequestMapping(path = "balance/{id}", method = RequestMethod.GET)
+    private BigDecimal getBalance(@PathVariable long userId){
+        return accountDao.checkBalance(userId);
+    }
+
+}
---
+++ capstone/tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -0,0 +1,32 @@
+package com.techelevator.tenmo.dao;
+
+import org.springframework.boot.autoconfigure.quartz.QuartzProperties;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.math.BigDecimal;
+
+@Component
+public class JdbcAccountDao implements AccountDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(DataSource ds) {
+        this.jdbcTemplate = new JdbcTemplate(ds);
+    }
+
+    @Override
+    public BigDecimal checkBalance(Long userID) {
+        String sql = "SELECT balance FROM account WHERE user_id = ?";
+        SqlRowSet results = this.jdbcTemplate.queryForRowSet(sql, userID);
+
+        BigDecimal balance= new BigDecimal("0.00");
+
+        if(results.next()){
+            balance = results.getBigDecimal("balance");
+        }
+        return balance;
+    }
+}
